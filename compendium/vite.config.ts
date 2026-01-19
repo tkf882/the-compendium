4a070de7414f041fb32c6551e7acda36ae4e0486
kotaro <trevorfavel882@gmail.com>
2026-01-19 15:33:37 -0800

    adjustjed base

--- compendium/vite.config.ts
+++ compendium/vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/the-compendium'
+  // base: '/the-compendium'
+  base: '/' // index is just /
 })
